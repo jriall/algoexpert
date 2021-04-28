81fbde4a1b6408783667a7b86adffbf44ad40995
James Riall <jamesrariall@gmail.com>
2021-04-28 14:00:43 +0100

    Add three largest numbers solution

---
+++ easy/three_largest_numbers.py
@@ -0,0 +1,27 @@
+# Find Three Largest Numbers
+# Write a function that takes in an array of at least three integers and,
+# without sorting the input array, returns a sorted array of the three largest
+# integers in the input array.
+
+# The function should return duplicate integers if necessary; for example, it
+# should return [10, 10, 12] for an input array of [10, 5, 9, 10, 12].
+
+import math
+
+def findThreeLargestNumbers(array):
+	results = {
+		'smallest': -math.inf,
+		'middle': -math.inf,
+		'largest': -math.inf,
+	}
+	for num in array:
+		if num > results['largest']:
+			results['smallest'] = results['middle']
+			results['middle'] = results['largest']
+			results['largest'] = num
+		elif num > results['middle']:
+			results['smallest'] = results['middle']
+			results['middle'] = num
+		elif num > results['smallest']:
+			results['smallest'] = num
+	return [results['smallest'], results['middle'], results['largest']]
