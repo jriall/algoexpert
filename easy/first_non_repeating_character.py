8312709e496fbc7128e9bc647406d3d381696693
James Riall <jamesrariall@gmail.com>
2021-04-28 14:44:55 +0100

    Add first non-repeating character solution

---
+++ easy/first_non_repeating_character.py
@@ -0,0 +1,21 @@
+# First Non-Repeating Character
+# Write a function that takes in a string of lowercase English-alphabet letters
+# and returns the index of the string's first non-repeating character.
+
+# The first non-repeating character is the first character in a string that
+# occurs only once.
+
+# If the input string doesn't have any non-repeating characters, your function
+# should return -1.
+
+def first_non_repeating_character(string):
+  counter = {}
+	for character in string:
+		if character in counter:
+			counter[character] += 1
+		else:
+			counter[character] = 1
+	for index, character in enumerate(string):
+		if counter[character] == 1:
+			return index
+    return -1
