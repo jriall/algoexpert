5a2afc7a9e294eac9bf5fc04abe6c1daef56bc73
James Riall <jamesrariall@gmail.com>
2021-05-03 20:35:31 +0100

    Add insertion sort solution

---
+++ easy/insertion_sort.py
@@ -0,0 +1,20 @@
+# Insertion Sort
+# Write a function that takes in an array of integers and returns a sorted
+# version of that array. Use the Insertion Sort algorithm to sort the array.
+
+# Sample Input
+# array = [8, 5, 2, 9, 5, 6, 3]
+# Sample Output
+# [2, 3, 5, 5, 6, 8, 9]
+
+# Solution
+
+def insertionSort(array):
+    for i in range(1, len(array)):
+		j = i
+		while j > 0 and array[j] < array[j - 1]:
+			temp = array[j]
+			array[j] = array [j - 1]
+			array[j - 1] = temp
+			j -= 1
+	return array
