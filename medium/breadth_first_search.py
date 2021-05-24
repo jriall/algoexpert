176f9f9e150b88f88fbe54e9d57336280bb8073e
James Riall <jamesrariall@gmail.com>
2021-05-24 21:58:54 +0100

    Add breadth first search solution

---
+++ medium/breadth_first_search.py
@@ -0,0 +1,45 @@
+# Breadth first search
+
+# You're given a Node class that has a name and an array of optional children
+# nodes. When put together, nodes form an acyclic tree-like structure.
+
+# Implement the breadthFirstSearch method on the Node class, which takes in an
+# empty array, traverses the tree using the Breadth-first Search approach
+# (specifically navigating the tree from left to right), stores all of the
+# nodes' names in the input array, and returns it.
+
+# If you're unfamiliar with Breadth-first Search, we recommend watching the
+# Conceptual Overview section of this question's video explanation before
+# starting to code.
+
+# Sample Input
+# graph = A
+#    /  |  \
+#   B   C   D
+#  / \   / \
+#   E   F   G   H
+#    / \   \
+#   I   J   K
+# Sample Output
+# ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
+
+# Solution
+
+class Node:
+  def __init__(self, name):
+    self.children = []
+    self.name = name
+
+  def addChild(self, name):
+    self.children.append(Node(name))
+    return self
+
+  def breadthFirstSearch(self, array):
+    queue = []
+    queue.append(self)
+    while len(queue):
+      current = queue.pop(0)
+      for child in current.children:
+        queue.append(child)
+      array.append(current.name)
+    return array
