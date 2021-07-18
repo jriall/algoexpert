fd18163e51c21fc2dc23cdd2edf5575289ac340d
James Riall <jamesrariall@gmail.com>
2021-07-18 17:20:25 +0100

    Add more efficient solution to Longest Substring Without Duplication

--- hard/longest_substring_without_duplication.py
+++ hard/longest_substring_without_duplication.py
@@ -11,7 +11,7 @@
 # Sample Output
 # "mentisac"
 
-# Solution
+# Infficient Solution O(n^2) time, O(n) space
 
 def longestSubstringWithoutDuplication(string):
   result = string[0]
@@ -28,3 +28,20 @@ def longestSubstringWithoutDuplication(string):
     if j - i > len(result):
       result = string[i:j]
   return result
+
+# O(n) time, O(n) space solution
+
+def longestSubstringWithoutDuplication(string):
+  seen = {}
+  result = {'start': 0, 'end': 1}
+  start = 0
+  for index, char in enumerate(string):
+    if char in seen:
+      start = max(start, seen[char] + 1)
+    if result['end'] - result['start'] < index + 1 - start:
+      result = {
+        'start': start,
+        'end': index + 1,
+      }
+    seen[char] = index
+  return string[result['start']:result['end']]
